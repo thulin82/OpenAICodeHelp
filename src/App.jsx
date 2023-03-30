0fa4c2e27b92b00499f987e27f2b5b7c58bc98a7
Markus Thulin <macky_b@hotmail.com>
2023-03-30 12:03:20 +0200

    Added fetching from openAI API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
+import { useState } from "react";
 import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TextInput from "./components/TextInput";
 
 const App = () => {
+    const [keywords, setKeywords] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const refactorCode = async (text) => {
+        setIsLoading(true);
+        setIsOpen(true);
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: "text-davinci-003",
+                prompt: "Refactor this code\n\n" + text + "",
+                temperature: 0.5,
+                max_tokens: 200,
+                frequency_penalty: 0.8,
+            }),
+        };
+        const response = await fetch(
+            import.meta.env.VITE_OPENAI_API_URL,
+            options
+        );
+        const json = await response.json();
+        const data = json.choices[0].text.trim();
+        console.log(data);
+        setKeywords(data);
+        setIsLoading(false);
+    };
+
     return (
         <Box bg="blue.600" color="white" height="100vh" padding={130}>
             <Container maxW="3xl" centerContent>
                 <Header />
-                <TextInput />
+                <TextInput refactorCode={refactorCode} />
                 <Footer />
             </Container>
         </Box>
